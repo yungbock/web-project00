3eb75e30a0ceba0c6941a937b7ff7d4f369672e9
yungbock <whs2769@gmail.com>
2024-04-08 21:35:23 +0900

    4/8 완료

---
+++ barter_Write/barter_Write.js
@@ -0,0 +1,11 @@
+const increase = (function(){
+    let number = 0;
+    return function (){
+        number +=1;
+        return number;
+    };
+})();
+function inCounter(){
+    document.getElementById("mainrecommend").innerText = increase();
+    // if ()
+}
